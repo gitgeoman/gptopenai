83bb0ea965380e624c06ad4aad71ab512de96dab
gitgeoman <45630165+gitgeoman@users.noreply.github.com>
2022-07-15 20:56:13 +0200

    CTA & blog part

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import './cta.css'
+import React from "react";
+import "./cta.css";
 
- const CTA = () => {
+const CTA = () => {
   return (
-    <div>CTA</div>
-  )
-}
-export default CTA
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Started </p>
+        <h3> Register today & start exlploration</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
+};
+export default CTA;

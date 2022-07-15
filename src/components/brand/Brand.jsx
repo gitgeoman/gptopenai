75b2acfbd5fd6bc004775cd905792b31cc65fd53
gitgeoman <45630165+gitgeoman@users.noreply.github.com>
2022-07-15 16:16:07 +0200

    brand bar

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import './brand.css'
+import React from "react";
+import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
-
-const brand = () => {
-  return (
-    <div>brand</div>
-  )
-}
-export default brand
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} alt="google" />
+    </div>
+    <div>
+      <img src={slack} alt="slack" />
+    </div>
+    <div>
+      <img src={atlassian} alt="atlassian" />
+    </div>
+    <div>
+      <img src={dropbox} alt="dropbox" />
+    </div>
+    <div>
+      <img src={shopify} alt="shopify" />
+    </div>
+  </div>
+);
+export default Brand;
